bc40f62b3f1116bc081bd238bb4832fc3b457eaa
katib1 <168632688+katib1@users.noreply.github.com>
2024-05-02 14:05:45 +0200

    routing of the form

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request #on importe ce que l'user a rempli dans la form
 
 app = Flask(__name__)
 
@@ -7,3 +7,8 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/play")
+def play():
+
+
+    return render_template("play.html")
